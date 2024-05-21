099f6475db203bc059baa733ab7ab1e10fb97986
Kimeunryeong <ker26772@gmail.com>
2024-05-21 11:14:58 +0900

    ce

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,18 @@ import { useNavigate } from "react-router-dom";
 export default function Header() {
   const navi = useNavigate();
   return (
-    <div className="w-full h-[60px] flex justify-center">
-      <div className="w-full max-w-[400px]  h-full flex items-center">
-        <div className="w-[20%] ml-4 flex justify-start items-center" onClick={() => navi(-1)}>
-          <IoIosArrowBack />
-        </div>
-        <img className="w-[15%] h-[50%]" src={headerLogo} alt="logo" />
-        <span className="text-2xl text-[#119724] font-semibold ffTitle">대구수목원</span>
+    <div className="w-full max-w-[400px] h-[60px] flex justify-center">
+      <div
+        className="ml-4 flex justify-start items-center"
+        onClick={() => navi(-1)}
+      >
+        <IoIosArrowBack />
+      </div>
+      <div className="w-full h-full flex items-center justify-center">
+        <img className=" mr-3 h-[50%]" src={headerLogo} alt="logo" />
+        <span className="text-2xl text-[#119724] font-semibold ffTitle">
+          대구수목원
+        </span>
       </div>
     </div>
   );
