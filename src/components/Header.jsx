c60587b67d40a48c7e0b0f448dd50c60fb5d01cf
ldh <ai04120@naver.com>
2024-05-22 09:46:17 +0900

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,12 @@ export default function Header() {
   const navi = useNavigate();
   return (
     <div className="w-full max-w-[400px] h-[60px] relative flex justify-center items-center">
-      <div
-        className="absolute top-6 left-5 z-10"
-        onClick={() => navi(-1)}
-      >
+      <div className="absolute top-6 left-5 z-10" onClick={() => navi(-1)}>
         <IoIosArrowBack />
       </div>
       <div className="absolute w-full h-full flex items-center justify-center">
         <img className="mr-2 h-[50%]" src={headerLogo} alt="logo" />
-        <span className="text-2xl text-[#119724] font-semibold ffTitle">
-          대구수목원
-        </span>
+        <span className="text-2xl text-[#119724] font-semibold ffTitle">대구수목원</span>
       </div>
     </div>
   );
--- src/components/IsLogin.js
+++ src/components/IsLogin.js
@@ -5,7 +5,7 @@ import Swal from "sweetalert2";
 
 const IsLogin = ({ updateUser }) => {
   const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true); // 로딩 상태를 관리합니다.
+  const [, setIsLoading] = useState(true); // 로딩 상태를 관리합니다.
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -25,11 +25,6 @@ const IsLogin = ({ updateUser }) => {
     setIsLoading(false); // 로딩 상태를 false로 업데이트합니다.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [navigate]); // useEffect 의존성 배열에 navigate와 updateUser 추가
-
-  // 로딩 중인 경우에는 로딩 메시지를 표시합니다.
-  if (isLoading) {
-    return <></>;
-  }
 
   // 사용자 데이터가 있으면 null을 반환합니다.
   return null;
